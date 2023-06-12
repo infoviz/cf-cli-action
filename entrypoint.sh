da1a7300364bffa3ce302b7814402ae81c9388f0
Ashish Singh <ashish.me@outlook.com>
2023-06-12 17:21:22 +0530

    Add option to support origin

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh -l
 
 cf api "$INPUT_CF_API"
-cf auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
 cf install-plugin multiapps -f
 
-if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_CF_SPACE" ]; then
+if [ -n "$INPUT_CF_ORIGIN_KEY" ]; then
+  cf login --origin $INPUT_CF_ORIGIN_KEY -a $INPUT_CF_API -u $INPUT_CF_USERNAME -p $INPUT_CF_PASSWORD -o $INPUT_CF_ORG -s $INPUT_CF_SPACE
+else
+  cf auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
   cf target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
 fi
 
